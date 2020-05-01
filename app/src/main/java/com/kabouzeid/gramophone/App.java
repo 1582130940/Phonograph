582af7468b87320413cf3f0ae69794ddd0bba64f
1582130940 <19632674+1582130940@users.noreply.github.com>
2020-05-02 02:04:51 +0800

    app: Clean up donation & pro logic
    * Enabled Pro version
    * Reference
    https://github.com/MoKee/android_packages_apps_Phonograph/commit/8237a07ac51539c0dfb1edd5819ea49066787bd8
    * Reference https://github.com/chr56/Phonograph_Plus

--- app/src/main/java/com/kabouzeid/gramophone/App.java
+++ app/src/main/java/com/kabouzeid/gramophone/App.java
@@ -11,9 +11,6 @@ import com.kabouzeid.gramophone.appshortcuts.DynamicShortcutManager;
  */
 public class App extends Application {
 
-    public static final String GOOGLE_PLAY_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjMeADN5Ffnt/ml5SYxNPCn8kGcOYGpHEfNSCts99vVxqmCn6C01E94c17j7rUK2aeHur5uxphZylzopPlQ8P8l1fqty0GPUNRSo18FCJzfGH8HZAwZYOcnRFPaXdaq3InyFJhBiODh2oeAcVK/idH6QraQ4r9HIlzigAg6lgwzxl2wJKDh7X/GMdDntCyzDh8xDQ0wIawFgvgojHwqh2Ci8Gnq6EYRwPA9yHiIIksT8Q30QyM5ewl5QcnWepsls7enNqeHarhpmSibRUDgCsxHoOpny7SyuvZvUI3wuLckDR0ds9hrt614scHHqDOBp/qWCZiAgOPVAEQcURbV09qQIDAQAB";
-    public static final String PRO_VERSION_PRODUCT_ID = "pro_version";
-
     private static App app;
 
     @Override
@@ -33,26 +30,6 @@ public class App extends Application {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
             new DynamicShortcutManager(this).initDynamicShortcuts();
         }
-    }
-
-    public static boolean isProVersion() {
-        return BuildConfig.DEBUG || app.billingProcessor.isPurchased(PRO_VERSION_PRODUCT_ID);
-    }
-
-    private static OnProVersionChangedListener onProVersionChangedListener;
-
-    public static void setOnProVersionChangedListener(OnProVersionChangedListener listener) {
-        onProVersionChangedListener = listener;
-    }
-
-    public static void notifyProVersionChanged() {
-        if (onProVersionChangedListener != null) {
-            onProVersionChangedListener.onProVersionChanged();
-        }
-    }
-
-    public interface OnProVersionChangedListener {
-        void onProVersionChanged();
     }
 
     public static App getInstance() {
--- app/src/main/java/com/kabouzeid/gramophone/ui/activities/AboutActivity.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/activities/AboutActivity.java
@@ -20,7 +20,6 @@ import com.kabouzeid.appthemehelper.ThemeStore;
 import com.kabouzeid.gramophone.App;
 import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.dialogs.ChangelogDialog;
-import com.kabouzeid.gramophone.dialogs.DonationsDialog;
 import com.kabouzeid.gramophone.ui.activities.base.AbsBaseActivity;
 import com.kabouzeid.gramophone.ui.activities.bugreport.BugReportActivity;
 import com.kabouzeid.gramophone.ui.activities.intro.AppIntroActivity;
@@ -79,8 +78,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
     LinearLayout reportBugs;
     @BindView(R.id.translate)
     LinearLayout translate;
-    @BindView(R.id.donate)
-    LinearLayout donate;
     @BindView(R.id.rate_on_google_play)
     LinearLayout rateOnGooglePlay;
     @BindView(R.id.aidan_follestad_git_hub)
@@ -142,7 +139,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
         writeAnEmail.setOnClickListener(this);
         translate.setOnClickListener(this);
         rateOnGooglePlay.setOnClickListener(this);
-        donate.setOnClickListener(this);
         aidanFollestadGitHub.setOnClickListener(this);
         michaelCookWebsite.setOnClickListener(this);
         maartenCorpelWebsite.setOnClickListener(this);
@@ -164,7 +160,7 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
 
     private static String getCurrentVersionName(@NonNull final Context context) {
         try {
-            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName + (App.isProVersion() ? " Pro" : "");
+            return context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
@@ -197,12 +193,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
             openUrl(TRANSLATE);
         } else if (v == rateOnGooglePlay) {
             openUrl(RATE_ON_GOOGLE_PLAY);
-        } else if (v == donate) {
-            if (App.isProVersion()) {
-                DonationsDialog.create().show(getSupportFragmentManager(), "DONATION_DIALOG");
-            } else {
-                startActivity(new Intent(this, PurchaseActivity.class));
-            }
         } else if (v == aidanFollestadGitHub) {
             openUrl(AIDAN_FOLLESTAD_GITHUB);
         } else if (v == michaelCookWebsite) {
